44b8ee60d8cc8408ab929f34dcd531c4148295b7
Samuel <olasam4liv@gmail.com>
2019-01-05 19:21:46 +0100

    add auto port to appjs and specify node version in package.json file

--- app.js
+++ app.js
@@ -283,8 +283,8 @@ app.post('/question', (req, res)=>{
         data: question
     })
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT)
 
-app.listen(3000, ()=>{
-    console.log(`server started on port 3000`)
-});
+console.log(`server started on port ${PORT}`)
 
